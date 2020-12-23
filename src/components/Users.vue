b6cf2bfa9d4622bb7c27e5f827a5f7f85324055a
ntold <nico.berchtold@me.com>
2020-12-23 18:58:49 +0100

    lastes changes

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,67 +1,71 @@
 <template>
-    <div class="users">
-        <h1>Users</h1>
-        <form v-on:submit="addUser">
-            <input type="text" v-model="newUser.name" placeholder="Enter Name">
-            <br />
-            <input type="text" v-model="newUser.email" placeholder="Enter Email">
-            <br />
-            <input type="submit" value="Submit">
-        </form>
-        <ul>
-            <li v-for="user in users">
-                <input type="checkbox" class="toggle" v-model="user.contacted">
-                <span :class="{contacted: user.contacted}">
-                    {{user.name}}: {{user.email}} <button v-on:click="deleteUser(user)">x</button>
-                </span>
-            </li>
-        </ul>
-        <li><router-link to="/Test">Test</router-link></li>
-    </div>
+  <div class="users">
+    <h1>Users</h1>
+    <form v-on:submit="addUser">
+      <input type="text" v-model="newUser.name" placeholder="Enter Name">
+      <br>
+      <input type="text" v-model="newUser.email" placeholder="Enter Email">
+      <br>
+      <input type="submit" value="Submit">
+    </form>
+    <ul>
+      <li v-for="user in users">
+        <input type="checkbox" class="toggle" v-model="user.contacted">
+        <span :class="{contacted: user.contacted}">
+          {{user.name}}: {{user.email}}
+          <button v-on:click="deleteUser(user)">x</button>
+        </span>
+      </li>
+    </ul>
+    <li>
+      <router-link to="/Test">Test</router-link>
+    </li>
+  </div>
 </template>
 
 <script>
-    import O from "./Test"
-    console.log(O.methods.greet)
-    export default {
-        name: 'users',
-        data() {
-            return {
-                newUser: {},
-                users: []
-
-            }
-        },
-        methods: {
-            addUser: function(e){
-                this.users.push({
-                    name: this.newUser.name,
-                    email: this.newUser.email,
-                    contacted: false
-                });
-                e.preventDefault();
-            },
-            deleteUser: function(user){
-                this.users.splice(this.users.indexOf(user), 1);
-            }
-        },
-        created: function(){
-            this.$http.get('https://jsonplaceholder.typicode.com/users')
-                .then(function(response){
-                    this.users = response.data;
-                })
-        }
+import O from "./Test";
+console.log(O.methods.greet);
+export default {
+  name: "users",
+  data() {
+    return {
+      newUser: {},
+      users: []
+    };
+  },
+  methods: {
+    addUser: function(e) {
+      this.users.push({
+        name: this.newUser.name,
+        email: this.newUser.email,
+        contacted: false
+      });
+      e.preventDefault();
+    },
+    deleteUser: function(user) {
+      this.users.splice(this.users.indexOf(user), 1);
     }
+  },
+  created: function() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(function(response) {
+        this.users = response.data;
+      });
+  }
+};
 </script>
 
 <style scoped>
-    .contacted{
-        text-decoration: line-through;
-    },
-    #app{
-         font-family: 'Avenir', Helvetica, Arial, sans-serif;
-         text-align: center;
-         color: #2c3e50;
-         margin-top: 10px;
-    }
+.contacted {
+  text-decoration: line-through;
+}
+,
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
 </style>
